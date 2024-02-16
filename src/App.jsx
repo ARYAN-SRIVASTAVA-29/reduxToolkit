c484764f32c2d72f674b9de45d7573683f09c48a
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-02-16 19:33:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AddToDo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Learn About Redux Toolkit</h1>
+    <AddToDo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
